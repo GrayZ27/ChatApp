b0ad53ebd14a3076f619f1e47b7094f166ec59f0
GrayZ27 <i520you.chen@qq.com>
2017-10-12 14:40:50 -0500

    setup SWReveal completed

---
+++ ChatApp/Controller/ChannelVC.swift
@@ -0,0 +1,22 @@
+//
+//  ChannelVC.swift
+//  ChatApp
+//
+//  Created by Gray Zhen on 10/12/17.
+//  Copyright © 2017 GrayStudio. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.revealViewController().rearViewRevealWidth = view.self.frame.size.width * 0.83
+        
+    }
+
+    
+
+}
---
+++ ChatApp/Controller/ChatVC.swift
@@ -0,0 +1,27 @@
+//
+//  ChatVC.swift
+//  ChatApp
+//
+//  Created by Gray Zhen on 10/12/17.
+//  Copyright © 2017 GrayStudio. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    //IBOutlets
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+        
+    }
+
+   
+
+}
