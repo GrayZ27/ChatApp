a23837a362d203f7f06328784d6bd933c7f0f83f
GrayZ27 <i520you.chen@qq.com>
2017-10-19 15:07:28 -0500

    built Login Page for users login

---
+++ ChatApp/Controller/LoginVC.swift
@@ -0,0 +1,23 @@
+//
+//  LoginVC.swift
+//  ChatApp
+//
+//  Created by Gray Zhen on 10/19/17.
+//  Copyright © 2017 GrayStudio. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+
+    @IBAction func closeBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
---
+++ ChatApp/Utilities/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  ChatApp
+//
+//  Created by Gray Zhen on 10/19/17.
+//  Copyright © 2017 GrayStudio. All rights reserved.
+//
+
+import Foundation
+
+let TO_LOGIN = "toLoginView"
