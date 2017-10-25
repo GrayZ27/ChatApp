bd45b340637591a3618283e32228112a17c38255
GrayZ27 <i520you.chen@qq.com>
2017-10-25 16:23:47 -0500

    set functions to load data on login page after register an user

--- ChatApp/Controller/ChannelVC.swift
+++ ChatApp/Controller/ChannelVC.swift
@@ -10,7 +10,9 @@ import UIKit
 
 class ChannelVC: UIViewController {
 
+    //IBOutlets
     @IBOutlet weak var loginBtn: UIButton!
+    @IBOutlet weak var userImg: UIImageView!
     
     @IBAction func prepareForUnwind(segue: UIStoryboardSegue) {}
     
@@ -18,7 +20,7 @@ class ChannelVC: UIViewController {
         super.viewDidLoad()
 
         self.revealViewController().rearViewRevealWidth = view.self.frame.size.width - 60
-        
+        NotificationCenter.default.addObserver(self, selector: #selector(ChannelVC.userDataDidChanged(_:)), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
     }
 
     @IBAction func loginBtnPressed(_ sender: Any) {
@@ -27,4 +29,17 @@ class ChannelVC: UIViewController {
     
     }
     
+    @objc func userDataDidChanged(_ notif: Notification) {
+        
+        if AuthService.instance.isLoggedIn {
+            loginBtn.setTitle(UserDataService.instance.name, for: .normal)
+            userImg.image = UIImage(named: UserDataService.instance.avatarName)
+            userImg.backgroundColor = UserDataService.instance.returnUIColor(components: UserDataService.instance.avatarColor)
+        }else {
+            loginBtn.setTitle("Login", for: .normal)
+            userImg.image = UIImage(named: "menuProfileIcon")
+            userImg.backgroundColor = UIColor.clear
+        }
+        
+    }
 }
