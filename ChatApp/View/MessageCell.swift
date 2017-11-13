17a65b2d22985447cddfd8c58e80c452ec4eca97
GrayZ27 <i520you.chen@qq.com>
2017-11-12 19:05:05 -0600

    added functions to show unread message and display message sending time

--- ChatApp/View/MessageCell.swift
+++ ChatApp/View/MessageCell.swift
@@ -28,6 +28,21 @@ class MessageCell: UITableViewCell {
         userImg.backgroundColor = UserDataService.instance.returnUIColor(components: message.userAvatarColor)
         messageBodyLbl.text = message.message
         userNameLbl.text = message.userName
+        
+        guard var isoDate = message.timeStamp else { return }
+        let end = isoDate.index(isoDate.endIndex, offsetBy: -5)
+        isoDate = String(isoDate[..<end])
+        
+        let isoFormatter = ISO8601DateFormatter()
+        let chatDate = isoFormatter.date(from: isoDate.appending("Z"))
+        
+        let newFormatter = DateFormatter()
+        newFormatter.dateFormat = "MMMd - h:mm a"
+        
+        if let finalDate = chatDate {
+            let finalDate = newFormatter.string(from: finalDate)
+            timeStampLbl.text = finalDate
+        }
     }
     
 }
